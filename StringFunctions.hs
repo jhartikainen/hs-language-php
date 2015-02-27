d32a26d93307a4161fd02611a54a648c448bed0e
haskellcamargo <marcelocamargo@linuxmail.org>
2015-02-26 23:49:01 -0300

    Implemented functions for string: strtoupper, strtolower

--- StringFunctions.hs
+++ StringFunctions.hs
@@ -4,9 +4,28 @@ import Tokenizer
 import Evaluator
 import Conversion
 import Control.Monad.Error
+import Data.Char
 
-functions = [("strlen", phpStrLen)]
+functions :: [(String, PHPFunctionType)]
+functions = [ ("strlen", phpStrLen)
+            , ("strtoupper", phpStrToUpper)
+            , ("strtolower", phpStrToLower)]
+
+-- Conversion of a PHPString for a Haskell [Char]
+toHaskellStr :: PHPValue -> String
+toHaskellStr x = stringFromPHPValue $ castToString x
+
+-- Throws an arity error
+arityErrorFor f = throwError $ Default $ "Wrong number of arguments to " ++ f
 
 phpStrLen :: PHPFunctionType
-phpStrLen (s:[]) = return $ PHPInt $ toInteger $ length $ stringFromPHPValue $ castToString s
-phpStrLen _ = throwError $ Default "Wrong number of arguments to strlen"
+phpStrLen (s:[]) = return $ PHPInt $ toInteger $ length $ toHaskellStr s
+phpStrLen _ = arityErrorFor "strlen"
+
+phpStrToUpper :: PHPFunctionType
+phpStrToUpper (s:[]) = return $ PHPString $ map toUpper $ toHaskellStr s
+phpStrToUpper _ = arityErrorFor "strtoupper"
+
+phpStrToLower :: PHPFunctionType
+phpStrToLower (s:[]) = return $ PHPString $ map toLower $ toHaskellStr s
+phpStrToLower _ = arityErrorFor "strtolower"
